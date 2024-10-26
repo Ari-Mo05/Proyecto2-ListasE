3c979ca1d5ccd734c15c7d93c76af2c0ee03f56a
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-26 16:46:22 -0600

    Update Principal.cpp

--- Proyecto2-borrador/Principal.cpp
+++ Proyecto2-borrador/Principal.cpp
@@ -2,17 +2,48 @@
 
 int main() {
 	int num, op;
+	ListaProfesores* listaP = new ListaProfesores();
+	ListaEst* listaE = new ListaEst();
+	ListaPer* listaPer = new ListaPer();
+	ListaCurso* listaC = new ListaCurso();
+	ListaGrupo* listaG = new ListaGrupo();
 
 	do {
 		num = Interfaz::menuPrincipal();
 		switch (num) {
-		case 1: {
-			op = Interfaz::subMenuAdministracion();
-		}
-		break;
-		case 2: {}break;
-		case 3: {}break;
-		case 4: {} break;
+			case 1: {
+				op = Interfaz::subMenuAdministracion();
+				switch (op) {
+					case 1: {
+						listaP = Interfaz::ingresaProfesor();
+					}break;
+					case 2: {
+						listaE = Interfaz::ingresaEstudiante();
+					} break;
+					case 3: {
+						listaPer = Interfaz::ingresaPeriodo();
+					} break;
+					case 4: {
+						listaC = Interfaz::ingresaCurso();
+					}break;
+					case 5: {
+						listaG = Interfaz::ingresaGrupo(listaC, listaP);
+					}break;
+					case 6: {
+						Interfaz::asignarProfesor(listaP, listaG);
+					}break;
+				}
+			}
+			break;
+			case 2: {
+				
+			}break;
+			case 3: {
+		
+			}break;
+			case 4: {
+		
+			} break;
 		}
 	} while (num!=5);
 
