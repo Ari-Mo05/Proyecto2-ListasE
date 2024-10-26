80a25ac3dcd1e3823196486db8295653889c020e
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-26 13:13:16 -0600

    Update Interfaz.h

--- Proyecto2-borrador/Interfaz.h
+++ Proyecto2-borrador/Interfaz.h
@@ -1,6 +1,29 @@
 #pragma once
+#include<iostream>
+#include<sstream>
+#include<fstream>
+using namespace std;
+
+
+#include"ListaEstudiantes.h"
+#include"ListaPeriodos.h"
+#include"ListaProfesores.h"
+#include"ListaCursos.h"
+#include"ListaGrupos.h"
+
 
 class Interfaz {
 	public:
-	
+		static int menuPrincipal();
+		static int subMenuAdministracion();
+		static ListaProfesores* ingresaProfesor();
+		static ListaEst* ingresaEstudiante();
+		static ListaPer* ingresaPeriodo();
+		static ListaCurso* ingresaCurso();
+		static ListaGrupo* ingresaGrupo();
+		static bool asignarProfesor(Profesor&);
+
+		static int subMenuMatricula();
+		static int subMenuBusquedaseInformes();
+		static int guardarDatosArchivos();
 };
