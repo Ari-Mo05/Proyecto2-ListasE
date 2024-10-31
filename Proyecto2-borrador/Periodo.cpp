a402f6452008fff570916685ab25c20a7bfc7a26
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-30 22:07:52 -0600

    Update Periodo.cpp

--- Proyecto2-borrador/Periodo.cpp
+++ Proyecto2-borrador/Periodo.cpp
@@ -2,16 +2,17 @@
 
 Periodo::Periodo(string inicio, string cierre){
     mesDeInicio = inicio;
-    cierre = cierre;
+    mesDeCierre = cierre;
 }
 Periodo::~Periodo(){
 }
 
 string Periodo::toString(){
-    stringstream p;
-    p << "------INFORMACION DE LOS PERIODOS------" << endl;
-    //...
-    return p.str();
+    stringstream s;
+    s << "------INFORMACION-DE-LOS-PERIODOS------" << endl;
+    s << "Mes de Inicio del ciclo: " << mesDeInicio << endl;
+    s << "Mes de Cierre del ciclo: " << mesDeCierre << endl;
+    return s.str();
 }
 
 string Periodo::getMesInicio()const {
