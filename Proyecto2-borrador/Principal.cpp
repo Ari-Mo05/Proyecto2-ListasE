7ce9dd68f0cbd5d75ef7b092c588061913c8e36d
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-31 10:36:01 -0600

    Update Principal.cpp

--- Proyecto2-borrador/Principal.cpp
+++ Proyecto2-borrador/Principal.cpp
@@ -1,80 +1,63 @@
-#include"Interfaz.h"
+#include"ListaProfesores.h"
 
-int main() {
-	int num, op;
-	ListaProfesores* listaP = new ListaProfesores();
-	ListaEst* listaE = new ListaEst();
-	ListaPer* listaPer = new ListaPer();
-	ListaCurso* listaC = new ListaCurso();
-	ListaGrupo* listaG = new ListaGrupo();
-	ListaMatri* listaM = new ListaMatri();
-
-	do {
-		num = Interfaz::menuPrincipal();
-		switch (num) {
-			case 1: {
-				op = Interfaz::subMenuAdministracion();
-				switch (op) {
-					case 1: {
-						listaP = Interfaz::ingresaProfesor(listaE);
-					}break;
-					case 2: {
-						listaE = Interfaz::ingresaEstudiante(listaE);
-					} break;
-					case 3: {
-						listaPer = Interfaz::ingresaPeriodo(listaPer);
-					} break;
-					case 4: {
-						listaC = Interfaz::ingresaCurso();
-					}break;
-					case 5: {
-						listaG = Interfaz::ingresaGrupo(listaC, listaP);
-					}break;
-					case 6: {
-						Interfaz::asignarProfesor(listaP, listaG);
-					}break;
-				}
-			}
-			break;
-			case 2: {
-				op = Interfaz::subMenuMatricula();
-				switch (op) {
-					case 1: {
-						listaM = Interfaz::matricularEstudiante(listaE, listaG);
-					}break;
-					case 2: {
-
-					}break;
-				}
-			}break;
-			case 3: {
-				op = Interfaz::subMenuBusquedaseInformes();
-				switch (op) {
-					case 1: {
-						Interfaz::profesoresRegistrados(listaP);
-					}break;
-					case 2: {
-						Interfaz::estudiantesRegistrados(listaE);
-					}break;
-					case 3: {}break;
-					case 4: {
-						Interfaz::profesorEspecifico(listaP);
-					} break;
-					case 5: {
-						Interfaz::periodosHabilitadosParaElAnnio(listaPer);
-					}break;
-					case 6: {}break;
-				}
-				Sleep(4000);
-			}break;
-			case 4: {
-		
-			} break;
+ListaProfesores::ListaProfesores() {
+	ppio = NULL;
+	pExt = NULL;
+}
+ListaProfesores::~ListaProfesores() {
+	delete ppio;
+	delete pExt;
+}
+bool ListaProfesores::agregarProfesor(Profesor& prof, ListaEst* listaE) {
+	if (!existeProfesor(prof.getId()) && !listaE->siExisteEstudiante(prof.getId())) {
+		if (ppio == NULL)
+			ppio = new NodoProfes(prof, pExt);
+		else {
+			if (ppio->getSiguiente() == NULL)
+				pExt = ppio;
+			while (pExt->getSiguiente() != NULL)
+				pExt = pExt->getSiguiente();
+			pExt->setSiguiente(new NodoProfes(prof, NULL));
 		}
-	} while (num!=5);
+		return true;
+	}
+	return false;
+}
+string ListaProfesores::mostrarProfesores() {
+	stringstream s;
+	pExt = ppio;
+	while (pExt != NULL) {
+		s << pExt->getProfe()->toString() << endl;
+		pExt = pExt->getSiguiente();
+	}
+	return s.str();
+}
+Profesor* ListaProfesores::obtenerProfesor(string ced) {
+	pExt = ppio;
+	while (pExt != NULL) {
+		if (pExt->getProfe()->getId() == ced)
+			return pExt->getProfe();
+		pExt = pExt->getSiguiente();
+	}
+	return NULL;
+}
 
+bool ListaProfesores::existeProfesor(string ced) {
+	pExt = ppio;
+	while (pExt != NULL) {
+		if (pExt->getProfe()->getId() == ced)
+			return true;
+		pExt = pExt->getSiguiente();
+	}
+	return false;
+}
 
-
-	system("pause");
-	return 0;
+string ListaProfesores::toString() {
+	stringstream s;
+	pExt = ppio;
+	while (pExt != NULL) {
+		s << pExt->getProfe()->toString() << endl;
+		pExt = pExt->getSiguiente();
+	}
+	return s.str();
 }
