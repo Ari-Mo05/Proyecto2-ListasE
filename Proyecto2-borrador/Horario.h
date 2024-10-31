c20d42a49b32dcbb4a11a459b7de1a5bf7b0ccce
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-30 22:05:42 -0600

    Update Horario.h

--- Proyecto2-borrador/Horario.h
+++ Proyecto2-borrador/Horario.h
@@ -3,22 +3,22 @@
 #include <sstream>
 using namespace std;
 
-//Hora de Inicio, Hora de Finalización, días de la semana
+//Hora de Inicio, Hora de FinalizaciÃ³n, dÃ­as de la semana
 class Horario {
 private:
 	int horaInicio;
 	int horaFinal;
-	string diasSemana;
+	string vec[7];
+	int tam; 
+	int can;
 	//puede tener mas datos...
 public:
-	Horario(int = 0, int = 0, string = "");
+	Horario(int = 0, int = 0, int = 0, int = 0);
 	virtual ~Horario();
+	void agregarDias(string);
 	void setHoraInicio(int);
 	void setHoraFinal(int);
-	void setDiasSemana(string);
 	int getHoraInicio()const;
 	int getHoraFinal()const;
-	string getDiasSemana()const;
-	//....
 	string toString()const;
 };
