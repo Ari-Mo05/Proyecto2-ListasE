598c2790c8351ee046962c53b229e4eaad22d735
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-31 10:08:29 -0600

    Update ListaEstudiantes.cpp

--- Proyecto2-borrador/ListaEstudiantes.cpp
+++ Proyecto2-borrador/ListaEstudiantes.cpp
@@ -16,6 +16,8 @@ bool ListaEst::ingresaEstudiante(Estudiante& estu){
 		if (ppio == NULL)
 			ppio = new NodoEst(estu, ppio);
 		else {
+			if (ppio->getSiguiente() == NULL)
+				actual = ppio;
 			while (actual->getSiguiente() != NULL)
 				actual = actual->getSiguiente();
 			actual->setSiguiente(new NodoEst(estu, NULL));
