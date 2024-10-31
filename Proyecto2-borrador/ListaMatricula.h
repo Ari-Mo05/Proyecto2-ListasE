ac6969258fed12b87236bda489a93f4c6ff93889
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-10-30 18:06:58 -0600

    Update ListaMatricula.h

--- Proyecto2-borrador/ListaMatricula.h
+++ Proyecto2-borrador/ListaMatricula.h
@@ -9,5 +9,10 @@ class ListaMatri {
 		ListaMatri();
 		virtual ~ListaMatri();
 		string mostrarMatricula();
+		bool agregarMatricula(Matricula&);
+		double calcularSubtotal(string);
+		int cantidadCursosPeriodo(int, string);
+		int cantidadCursosAnual(string);
+		double descuento(string);
 		string factura(string);
 };
