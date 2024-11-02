5bd5fbbad3f6c1eb6972e1bf1ae8cbb35713a76d
mmadrigal56 <mariana.madrigal.molina@est.una.ac.cr>
2024-11-02 09:54:55 -0600

    Update Periodo.cpp

--- Proyecto2-borrador/Periodo.cpp
+++ Proyecto2-borrador/Periodo.cpp
@@ -9,9 +9,26 @@ Periodo::~Periodo(){
 
 string Periodo::toString(){
     stringstream s;
-    s << "--------INFORMACION-DEL-PERIODO--------" << endl;
-    s << "Mes de Inicio del ciclo: " << mesDeInicio << endl;
-    s << "Mes de Cierre del ciclo: " << mesDeCierre << endl;
+    if (mesDeInicio == "Enero") {
+        s << "Periodo 1:" << endl;
+        s << "Mes de Inicio del ciclo: " << mesDeInicio << endl;
+        s << "Mes de Cierre del ciclo: " << mesDeCierre << endl;
+    }
+    if (mesDeInicio == "Abril") {
+        s << "Periodo 2: " << endl;
+        s << "Mes de Inicio del ciclo: " << mesDeInicio << endl;
+        s << "Mes de Cierre del ciclo: " << mesDeCierre << endl;
+    }
+    if (mesDeInicio == "Julio") {
+        s << "Periodo 3: " << endl;
+        s << "Mes de Inicio del ciclo: " << mesDeInicio << endl;
+        s << "Mes de Cierre del ciclo: " << mesDeCierre << endl;
+    }
+    if (mesDeInicio == "Octubre") {
+        s << "Periodo 4: " << endl;
+        s << "Mes de Inicio del ciclo: " << mesDeInicio << endl;
+        s << "Mes de Cierre del ciclo: " << mesDeCierre << endl;
+    }
     return s.str();
 }
 
